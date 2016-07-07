e9828d0e1912f4e146bce6ff44dc0b7c48aae2f2
Fernando Almeida <lfalmeida@me.com>
2016-07-06 22:04:09 -0300

    adicionando task para geracao de modulo bluenote

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     apidoc = require('gulp-apidoc'),
     phpunit = require('gulp-phpunit'),
     run = require('gulp-run'),
-    guppy = require('git-guppy')(gulp);
+    guppy = require('git-guppy')(gulp),
+    prompt = require('gulp-prompt');
 
 /**
  *
@@ -11,8 +12,7 @@ gulp.task('apidocs', function (done) {
     return apidoc({
         src: "app/Http/Controllers",
         dest: "public/docs/api",
-        config: './',
-
+        config: './'
     }, done);
 });
 
@@ -46,12 +46,28 @@ gulp.task('phpunit', function () {
     });
 });
 
+gulp.task('bluenote', function () {
+    process.chdir('public/admin/app/modules');
+
+    gulp.src('')
+        .pipe(prompt.prompt({
+            type: 'input',
+            name: 'moduleName',
+            message: 'Como será o nome do módulo (lowerCamelCase) ?'
+        }, function (r) {
+            // process.stdout.write(r);
+            var command = 'yo bluenote ' + r.moduleName +' --skip-install';
+            return run(command).exec();
+        }));
+});
+
+
 /**
  *
  */
 gulp.task('docs', ['apidocs', 'sami-docs']);
 
 /**
- * 
+ *
  */
 gulp.task('pre-commit', ['phpunit', 'docs']);
